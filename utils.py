a12202926d35b73da0107bc1937bc2bf05764938
Ziolo03 <fifiziolo@gmial.com>
2025-05-14 12:21:09 +0200

    321

--- utils.py
+++ utils.py
@@ -1,16 +1,20 @@
 """Module providing basic calculator operations."""
 
+
 def add(a, b):
     """Return the sum of a and b."""
     return a + b
+
 
 def subtract(a, b):
     """Return the difference of a and b."""
     return a - b
 
+
 def multiply(a, b):
     """Return the product of a and b."""
     return a * b
+
 
 def divide(a, b):
     """Return the division of a by b."""
